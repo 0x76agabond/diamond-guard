39048ab5d2b02f6b9b26ba73189c7935f8c0f9d3
Vagabond <ginz1504@gmail.com>
2025-12-31 23:29:10 +0700

    refactor context

--- src/guardContext/libraries/LibContext.sol
+++ src/guardContext/libraries/LibContext.sol
@@ -13,9 +13,9 @@ pragma solidity >=0.8.30;
 library LibContext {
     // EIP-1153
     // Storage slots for context data
-    uint256 internal constant SLOT_NONCE = 0;
-    uint256 internal constant SLOT_TXHASH = 1;
-    uint256 internal constant SLOT_TX_TYPE = 2;
+    bytes32 internal constant SLOT_NONCE = keccak256("vag.context.nonce");
+    bytes32 internal constant SLOT_TXHASH = keccak256("vag.context.txhash");
+    bytes32 internal constant SLOT_TX_TYPE = keccak256("vag.context.txtype");
 
     // Safe workflow
     // checkTransaction (Guard) => executeTransaction (Safe) => checkAfterExecution (Guard)
@@ -25,41 +25,47 @@ library LibContext {
 
     // Getters
     function getNonce() internal view returns (uint256 v) {
+        bytes32 position = SLOT_NONCE;
         assembly {
-            v := tload(SLOT_NONCE)
+            v := tload(position)
         }
     }
 
     function getTxHash() internal view returns (bytes32 v) {
+        bytes32 position = SLOT_TXHASH;
         assembly {
-            v := tload(SLOT_TXHASH)
+            v := tload(position)
         }
     }
 
     // 0 = normal tx
     // 1 = module tx
     function getTxType() internal view returns (uint256 v) {
+        bytes32 position = SLOT_TX_TYPE;
         assembly {
-            v := tload(SLOT_TX_TYPE)
+            v := tload(position)
         }
     }
 
     //  Setters
     function setNonce(uint256 v) internal {
+        bytes32 position = SLOT_NONCE;
         assembly {
-            tstore(SLOT_NONCE, v)
+            tstore(position, v)
         }
     }
 
     function setTxHash(bytes32 v) internal {
+        bytes32 position = SLOT_TXHASH;
         assembly {
-            tstore(SLOT_TXHASH, v)
+            tstore(position, v)
         }
     }
 
     function setTxType(uint256 v) internal {
+        bytes32 position = SLOT_TX_TYPE;
         assembly {
-            tstore(SLOT_TX_TYPE, v)
+            tstore(position, v)
         }
     }
 }
